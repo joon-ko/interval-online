704d72b8a0412dc0135dbe4b2189f7190f2afd4d
joon ko <joonhok@mit.edu>
2020-06-04 19:18:02 -0400

    drag soundblocks

--- public/src/index.ts
+++ public/src/index.ts
@@ -1,9 +1,4 @@
-import {
-  draw,
-  onMouseDown,
-  onMouseMove,
-  onMouseUp
-} from './modules/draw.js'
+import { soundBlockHandler } from './modules/draw.js'
 
 const canvas = <HTMLCanvasElement> document.getElementById('canvas')
 const ctx = <CanvasRenderingContext2D> canvas.getContext('2d')
@@ -15,12 +10,13 @@ const resizeCanvas = (): void => {
 resizeCanvas()
 
 window.addEventListener('resize', resizeCanvas)
-
-canvas.addEventListener('mousedown', onMouseDown)
-canvas.addEventListener('mousemove', onMouseMove)
-canvas.addEventListener('mouseup', onMouseUp)
+document.addEventListener('keydown', soundBlockHandler.onKeyDown)
+document.addEventListener('keyup', soundBlockHandler.onKeyUp)
+canvas.addEventListener('mousedown', soundBlockHandler.onMouseDown)
+canvas.addEventListener('mousemove', soundBlockHandler.onMouseMove)
+canvas.addEventListener('mouseup', soundBlockHandler.onMouseUp)
 
 window.setInterval(() => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  draw()
+  soundBlockHandler.draw()
 }, 1000/60)
--- public/src/modules/draw.ts
+++ public/src/modules/draw.ts
@@ -1,10 +1,3 @@
-let blocks: SoundBlock[] = [];
-
-let mousedown: boolean = false;
-let holdPoint: Point = null;
-let holdSize: Size = {width: 0, height: 0};
-let holdColor: string = null;
-
 const canvas = <HTMLCanvasElement> document.getElementById('canvas');
 const ctx = <CanvasRenderingContext2D> canvas.getContext('2d');
 
@@ -19,7 +12,10 @@ interface Size {
 }
 
 class SoundBlock {
-  pos: Point; size: Size; color: string;
+  pos: Point;
+  size: Size;
+  color: string;
+
   constructor(pos: Point, size: Size, color: string) {
     this.pos = pos;
     this.size = size;
@@ -33,54 +29,155 @@ class SoundBlock {
   }
 }
 
-const getCursorPosition = (canvas: HTMLCanvasElement, e: MouseEvent): Point => {
-  const rect = canvas.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-  return {x: x, y: y};
-}
+class SoundBlockHandler {
+  blocks: SoundBlock[];
 
-const onMouseDown = (e: MouseEvent): void => {
-  const pos = getCursorPosition(canvas, e);
-  holdColor = `rgb(
-    ${Math.floor(Math.random() * 255)},
-    ${Math.floor(Math.random() * 255)},
-    ${Math.floor(Math.random() * 255)}
-  )`;
-  holdPoint = pos;
-  mousedown = true;
-}
+  mousedown: boolean;
+  keydown: boolean;
 
-const onMouseMove = (e: MouseEvent): void => {
-  if (!mousedown) return;
-  const cursorPos = getCursorPosition(canvas, e);
-  holdSize = {
-    width: cursorPos.x - holdPoint.x,
-    height: cursorPos.y - holdPoint.y
-  };
-}
+  holdPoint: Point;
+  holdSize: Size;
+  holdColor: string;
 
-const onMouseUp = (e: MouseEvent): void => {
-  if (!mousedown) return;
-  blocks.push(new SoundBlock(holdPoint, holdSize, holdColor));
-  mousedown = false;
+  dragBlock: SoundBlock;
+  dragPoint: Point;
+  originalPoint: Point;
+  dragSize: Size;
 
-  holdPoint = null;
-  holdSize = {width: 0, height: 0};
-  holdColor = null;
-}
+  constructor() {
+    this.blocks = [];
 
-const drawHold = (): void => {
-  ctx.beginPath();
-  ctx.fillStyle = holdColor;
-  ctx.fillRect(holdPoint.x, holdPoint.y, holdSize.width, holdSize.height);
-}
+    this.mousedown = false;
+    this.keydown = false;
 
-const draw = (): void => {
-  for (let block of blocks) {
-    block.draw();
+    this.holdPoint = null;
+    this.holdSize = {width: 0, height: 0};
+    this.holdColor = null;
+
+    this.dragBlock = null;
+    this.dragPoint = null;
+    this.originalPoint = null;
+    this.dragSize = {width: 0, height: 0};
   }
-  if (holdPoint !== null) drawHold();
+
+  /* Gets the cursor position on the given canvas. */
+  getCursorPosition = (canvas: HTMLCanvasElement, e: MouseEvent): Point => {
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    return {x: x, y: y};
+  }
+
+  /* Returns the earliest deployed SoundBlock that the mouse is currently over, otherwise returns
+   * null. */
+  getMousedOverBlock = (cursor: Point): SoundBlock => {
+    for (let block of this.blocks) {
+      const dx = cursor.x - block.pos.x;
+      const dy = cursor.y - block.pos.y;
+      if ((0 < dx && dx < block.size.width) && (0 < dy && dy < block.size.height)) {
+        return block;
+      }
+    }
+    return null;
+  }
+
+  /* Given a start and end points that uniquely determine a rectangle, returns the top-left corner
+   * and size of the rectangle. */
+  normalizeRectangle = (start: Point, end: Point): [Point, Size] => {
+    const dx = end.x - start.x;
+    const dy = end.y - start.y;
+
+    if      (dx >= 0 && dy >= 0) return [start, {width: dx, height: dy}];
+    else if (dx >= 0 && dy < 0)  return [{x: start.x, y: end.y}, {width: dx, height: -dy}];
+    else if (dx < 0  && dy >= 0) return [{x: end.x, y: start.y}, {width: -dx, height: dy}];
+    else if (dx < 0  && dy < 0)  return [end, {width: -dx, height: -dy}];
+  }
+
+  onMouseDown = (e: MouseEvent): void => {
+    this.mousedown = true;
+    const cursor = this.getCursorPosition(canvas, e);
+    if (!this.keydown) {
+      this.holdColor = `rgb(
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)}
+      )`;
+      this.holdPoint = cursor;
+    } 
+    else {
+      this.dragBlock = this.getMousedOverBlock(cursor);
+      if (this.dragBlock !== null) {
+        this.dragPoint = cursor;
+        this.originalPoint = this.dragBlock.pos;
+      }
+    }
+  }
+
+  onMouseMove = (e: MouseEvent): void => {
+    const cursor = this.getCursorPosition(canvas, e);
+    if (!this.mousedown) return;
+    if (this.dragBlock === null && this.holdPoint !== null) {
+      this.holdSize = {
+        width: cursor.x - this.holdPoint.x,
+        height: cursor.y - this.holdPoint.y
+      };
+    }
+    else if (this.dragBlock !== null) {
+      this.dragSize = {
+        width: cursor.x - this.dragPoint.x,
+        height: cursor.y - this.dragPoint.y
+      };
+      this.dragBlock.pos = {
+        x: this.originalPoint.x + this.dragSize.width,
+        y: this.originalPoint.y + this.dragSize.height
+      };
+    }
+  }
+
+  onMouseUp = (e: MouseEvent): void => {
+    const cursor = this.getCursorPosition(canvas, e);
+    if (!this.mousedown) return;
+    if (this.dragBlock === null && this.holdPoint !== null) {
+      const [normPoint, normSize] = this.normalizeRectangle(this.holdPoint, cursor)
+      this.blocks.push(new SoundBlock(normPoint, normSize, this.holdColor));
+      this.mousedown = false;
+      this.holdPoint = null;
+      this.holdSize = {width: 0, height: 0};
+      this.holdColor = null;
+    }
+    else if (this.dragBlock !== null) {
+      this.dragBlock = null;
+      this.dragPoint = null;
+      this.originalPoint = null;
+      this.dragSize = {width: 0, height: 0};
+    }
+    this.mousedown = false;
+  }
+
+  onKeyDown = (e: KeyboardEvent): void => {
+    if (e.keyCode === 32) this.keydown = true;
+  }
+
+  onKeyUp = (e: KeyboardEvent): void => {
+    if (e.keyCode === 32) this.keydown = false;
+  }
+
+  drawHold = (): void => {
+    ctx.beginPath();
+    ctx.fillStyle = this.holdColor;
+    ctx.fillRect(
+      this.holdPoint.x, this.holdPoint.y, this.holdSize.width, this.holdSize.height
+    );
+  }
+
+  draw = (): void => {
+    for (let block of this.blocks) {
+      block.draw();
+    }
+    if (this.holdPoint !== null) this.drawHold();
+  }
 }
 
-export { draw, onMouseDown, onMouseMove, onMouseUp }
+const soundBlockHandler = new SoundBlockHandler();
+
+export { soundBlockHandler }
